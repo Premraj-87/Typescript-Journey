594927bae5c285cd7a0333f918da16ed95109fad
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-14 18:00:06 +0530

    understand tuple

--- 2 Array & Tuples/practice.js
+++ 2 Array & Tuples/practice.js
@@ -1,60 +1,45 @@
+"use strict";
 //* Practice Questions
 //!Create number array:
 //* [10,20,30,40]
-
 // {
 //     let num:number[]=[10,20,30,40];
 //     num.push(50)
 //     console.log(num)
 // }
-
 //! 2. Create String array and print all name using loop
-
 // {
 //    let names: string[] = ["Prem", "Raj", "Amit", "Anand"];
-
 // // For loop
 // // for (let i = 0; i < names.length; i++) {
 // //   console.log(names[i]);
 // // }
-
 // for(let name of names){
 //     console.log(name);
 // }
-
 // }
-
 //! 3. Create Tuple
 // //* [id,username]
 // {
 //     let user:[number,string]=[1,"prem"]
 //     console.log(user);
-    
 // }
-
 //! 4. Create readonly RGB tuple
 // {
 //     let rgb:readonly[number,number,number]=[255,0,0]
 //     console.log(rgb)
 // }
-
 //! 5. Create optional tuple
 // //* [name,age?]
 // {
 //     let user1:[string,number?]
 //     user1 = ["Prem"]
 //     console.log(user1)
-
 // }
-
 //! 6.Create tuple 
-// //*[id,name,isAdmin]
-
-// {
-//    let user:[number,string,boolean];
-//    user =[1,"prem",true]
-//    console.log(user)
-// }
-
-//! 7.Create array
-
+//*[id,name,isAdmin]
+{
+    let user;
+    user = [1, "prem", true];
+    console.log(user);
+}
