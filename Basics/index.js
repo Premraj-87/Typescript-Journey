4302025ecf44eedd953bc90fdd2685f487bb6637
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-14 15:34:29 +0530

    Learn basic

--- Basics/index.js
+++ Basics/index.js
@@ -1,28 +1,31 @@
+"use strict";
 // //! 1. Task :Fix the errors
-
 // let age: number = 25;
 // let nam: string = "Mack";
 // let isLoggedIn: boolean = true;
-
 // //! 2. Identify types
-
 // let a = 10;        // number ✅
 // let b = "Hello";   // string ✅
 // let c = true;      // boolean ✅
 // let d = null;      // ❌
 // let e;             // undefined ❌
-
 // //! 3.Output prediction
-
 // let data: unknown = "TypeScript";
-
 // if (typeof data === "string") {
 //   console.log(data.length);
 // }
 // //ans =10
-
 // //! 4. Function type
 // function add(a: number, b: number):number {
 //   return a + b;
 // }
 
+
+//* 1.Variable Typing
+let score = 95;
+let username = "Prem";
+let isAdmin = false;
+//* 2.Fix the errors
+let age = 20;
+let name = "Anand";
+let active = true;
