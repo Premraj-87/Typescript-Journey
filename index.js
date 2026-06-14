9fa074fe481ae1a0c2fd4100b6ccce858454032c
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-14 14:50:38 +0530

    started basic learning

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//@ts-ignore
-let message = "Hello this is String";
-console.log(message);
-let sentence = "please this is it";
-let sentenceLength = sentence.length;
-console.log(sentenceLength);
-//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,6 +0,0 @@
-//@ts-ignore
-let message:string = "Hello this is String";
-console.log(message)
-let sentence :string ="please this is it";
-let sentenceLength =sentence.length;
-console.log(sentenceLength)
