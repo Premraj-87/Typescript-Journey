a2c928d61e93d320b2348c52d9ddc79c279c86e3
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-14 09:23:40 +0530

    First day

--- app.cpp
+++ app.cpp
@@ -1,8 +1,1 @@
-#include <iostream>  // Input-output stream library
-
-using namespace std; // To avoid writing std:: before cout
-
-int main() {
-    cout << "Hello, World!" << endl; // Print message
-    return 0; // Exit program successfully
-}
+//hello
