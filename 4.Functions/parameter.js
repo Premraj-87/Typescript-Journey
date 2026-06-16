f5fb413ab8c2b2844b47f397e68a031b2e76a92d
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-06-16 09:30:13 +0530

    parameter type completed

--- 4.Functions/parameter.js
+++ 4.Functions/parameter.js
@@ -1,21 +1,18 @@
-//! 1.Parameter Types 
-
+"use strict";
+//* 1.Parameter Types 
 // {
 //     function add(a:number,b:number){
 //         return a+b;
 //     }
-
 //     console.log(add(5,4))
 // }
-
 //* Practice 1
 // function multiphy(a:number,b:number){
 //     return a *b;
 // }
 // console.log(multiphy(5,4))
-
 //* Practice 2
-// function greet(name:string){
-//     return `Hello ${name}`;
-// }
-// greet("Prem")
+function greet(name) {
+    return `Hello ${name}`;
+}
+greet("Prem");
